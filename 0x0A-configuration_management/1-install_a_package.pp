98b3f0a0fbb0d298b781824c642ca3a4707e4602
root <kamamoctar@gmail.com>
2023-12-11 17:38:09 +0000

    updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install precise version of flask the 210
+# Installing flask
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
